12eccbc2efb49b2355733dbbc4e58a48ae1d026a
anton.svirepa-idf <anton.svirepa@moneyman.ru>
2021-06-08 10:23:28 +0300

    feat(01): added hello world get controller

--- src/main/java/com/project/bot/controller/IncomingMessageController.java
+++ src/main/java/com/project/bot/controller/IncomingMessageController.java
@@ -1,6 +1,7 @@
 package com.project.bot.controller;
 
 import com.project.bot.model.IncomingMessage;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/bot")
 public class IncomingMessageController {
+
+  @GetMapping
+  public String get() {
+    return "Hello world!";
+  }
 
   @PostMapping
   public String post(@RequestBody IncomingMessage incomingMessage) {
